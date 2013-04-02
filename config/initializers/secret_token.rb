d68ac00c22cbe1a158a44c4cbb7b924e3dfd5672
Amandeep <amandeep.bhamra@gmail.com>
2013-04-02 15:23:35 +0530

    git initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxTry::Application.config.secret_token = '8459eb821ad79e9a47c49cd87b83fbcf342a0aa7572f9b31c7d6c8dc8af568abe277c0460730318e8b7bf73a888d2dd315ac3df3de47bc980a1d2bb8ef5df17c'
